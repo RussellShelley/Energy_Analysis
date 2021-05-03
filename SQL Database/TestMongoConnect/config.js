da1a1530cb89e4c2bcf52fc343dc215c4fca07c5
DRUiz <danieleyt@hotmail.com>
2021-05-02 20:26:12 -0700

    test mongodb connection for station data

---
+++ SQL Database/TestMongoConnect/config.js
@@ -0,0 +1,5 @@
+const station_api=YIuUzIE6Y7B5B3L8HWGiSjZ6CboIngAgJ3hnnmj8
+
+const uri = "mongodb+srv://cluster0.wog5x.mongodb.net/EV_Database" --username team1_ea
+
+const mongodb_password = tYi34v8yZJaBODeB
